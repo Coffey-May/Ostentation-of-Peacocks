eab4bff6e80f855f759e1e7e8867db6002ab85a0
ETamez9173 <elitamez17@gmail.com>
2020-01-31 10:25:56 -0600

    Friend Delete Button working, FriendList rendering properly

--- src/components/friends/Friend.js
+++ src/components/friends/Friend.js
@@ -1,35 +1,38 @@
-import React from "react"
-// import React, { useContext } from "react"
+// import React from "react"
+import React, { useContext } from "react"
 // import { FriendContext } from "./FriendProvider"
 // import "./Friend.css"
 
+// const { friends, deleteFriend } = useContext(FriendContext)
+
+// if functions have more than one line of logic - that logic must be wrapped in curly {}
+//  and any returns must be explicit
+export default ({ friend, deleteFriend }) => {
+
+// deleteFriend was passed from FriendList.js and now can be called as a function 
+
+    // console.log(friend)
+    return (
+        <section className="friend">
+            <h3 className="friend__name">Friend: {friend.user.userName}</h3>
 
 
 
-export default ({ friend, customer }) => (
-    <section className="friend">
-        <h3 className="friend__name">Friend: {friend.userName}</h3>
-        {/* <h3 className="friend__name">fFriend: {friend.user.userName}</h3> */}
-        {/* <h3 className="friend__name">cFriend: {customer.userName}</h3> */}
-        {/* <div className="employee__location">{location.name}</div> */}
-        {/* <div className="location__locationId">Location {loaction.locationId}</div> */}
-    
-    
-    
-        <button onClick={
+            <button onClick={
                 () => {
-                    // deleteFriend(friend)
-                    //     .then(() => {
-                    //         props.history.push("/friends")
-                    //     })
+                    deleteFriend(friend)
+                        // .then(() => {
+                        //     props.history.push("/friends")
+                        // })
                 }
             }>
                 Delete Friend
             </button>
-    
-    </section>
+
+        </section>
 
 
 
-)
+    )
+}
 
--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -4,7 +4,7 @@
 import React, { useContext } from "react"
 
 import { FriendContext } from "./FriendProvider"
-import { UserContext } from "../auth/UserProvider"
+// import { UserContext } from "../auth/UserProvider"
 
 import Friend from "./Friend"
 
@@ -12,18 +12,16 @@ import "./Friend.css"
 
 export default (props) => {
 
-    const { friends } = useContext(FriendContext)
-    // const { users } = useContext(UserContext)
-    // const friendGroup1 = friends
+    const { friends, deleteFriend } = useContext(FriendContext)
+   // when render friend componnet will 
+   
     // console.log(friends)
-    // const { users } = useContext(userContext)
-
+ 
     const activeUser = localStorage.getItem("nutshell_user")
-    // const userGroup = users
+   
     console.log(activeUser)
     // debugger
-    // const userGroup = users
-    // debugger
+  
     return (
 
 
@@ -34,72 +32,19 @@ export default (props) => {
                 Add Friend
             </button>
 
-
             <article className="friendList">
 
-
-
-
                  {
-
-
-                    friends.map(friend => {
-                        const friendsOfActiveUser = friends.filter(friend => parseInt(activeUser) === friend.initiatorId)
+                    friends.filter(friend => parseInt(activeUser) === friend.initiatorId).map(friend => {
+                        return <Friend key={friend.id} friend={friend} deleteFriend={deleteFriend}/>
+                    })
                         // const friendsOfActiveUser = friends.filter(friend => activeUser === friend.userName)
                       
-                        console.log(friend.initiatorId)
-                        console.log(friend.user.userName)
-                        
-                        console.log(activeUser)
-                        console.log(friendsOfActiveUser)
-                        
-                        // return <Friend key={friend.userName}
-                        // return <Friend key={friendsOfActiveUser.userName}
-                        // return <Friend key={friendsOfActiveUser.userId}
-                        // return <Friend key={friendsOfActiveUser.id}
-                        // return <Friend key={friendsOfActiveUser.initiatorId}
-                        // return <Friend key={friend.initiatorId}
-                        // return <Friend key={friend.initiatorId}
-                        // return <Friend key={friend.userId}
-                        // return <Friend key={friend.user.Id}
-                        // return <Friend key={activeUser}
-                            // friend={friendsOfActiveUser}
-                              return <Friend key={friend.user.userName}
-
-                            friend={friendsOfActiveUser}
-                            // customer={friend}
-
-                        />
-
-
-                    })
+                    
                 }
 
 
-
-                {/* {
-
-
-                    friends.filter(friend => {
-                        const friendsOfActiveUser = (f => activeUser === f.initiatorId).map
-                        // const friendFound = users.find(l => l.id === friend.locationId)
-
-                        return <Friend key={friend.userName}
-
-                            friend={friendsOfActiveUser}
-                            customer={friend}
-
-                        />
-
-                    })
-                } */}
-
-
-
-
             </article>
-
-
 
         </div>
     )
