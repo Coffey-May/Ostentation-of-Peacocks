e9ae66eade30c89e72a62f9ef94db3822486b1fb
rebeccapatek <rebeccapatek@gmail.com>
2020-01-31 10:48:48 -0600

    got coffey's

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -22,19 +22,19 @@ import { ArticleProvider } from "./articles/ArticleProvider"
 // import { ChatProvider } from "./chat/ChatProvider"
 // import { EventProvider } from "./event/EventProvider"
 // import { FriendProvider } from "./friend/FriendProvider"
-// import { TaskProvider } from "./task/TaskProvider"
+import { TaskProvider } from "./tasks/TaskProvider"
 import ArticleList from "./articles/ArticleList"
 import Login from "./auth/Login"
 // import ChatList from "./chats/ChatList"
 // import EventList from "./events/EventList"
 // import FriendList from "./friends/FriendList"
-// import TaskList from "./tasks/TaskList"
+import TaskList from "./tasks/TaskList"
 import ArticleForm from "./articles/ArticleForm"
 // import ChatForm from './chats/ChatForm'
 // import EventForm from "./events/EventForm"
 // import FriendForm from './friends/FriendForm'
 import TaskForm from './tasks/TaskForm'
-import { UserProvider } from "./auth/UserProvider"
+
 
 
 
