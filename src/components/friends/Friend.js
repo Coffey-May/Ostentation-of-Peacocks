6b4e4d60ccd3b8eb734e25b11800a50f2cde6d7a
AdiGarmendia <dejima@gmail.com>
2020-02-03 10:55:37 -0600

    what

--- src/components/friends/Friend.js
+++ src/components/friends/Friend.js
@@ -1,16 +1,16 @@
 // import React from "react"
 import React, { useContext } from "react"
 import { UserContext } from "../auth/UserProvider"
-import { FriendContext } from "./FriendProvider"
+// import { FriendContext } from "./FriendProvider"
 // import { FriendContext } from "./FriendProvider"
 // import "./Friend.css"
 
-const { friends, deleteFriend } = useContext(FriendContext)
+// const { friends, deleteFriend } = useContext(FriendContext)
 
 // if functions have more than one line of logic - that logic must be wrapped in curly {}
 //  and any returns must be explicit
 export default ({ friend }) => {
-    const { deleteFriend } = useContext(FriendContext)
+    // const { deleteFriend } = useContext(FriendContext)
 
 // deleteFriend was passed from FriendList.js and now can be called as a function 
 
