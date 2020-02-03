d937fd8e329f21f9540faa79a5d56f5aa825d25f
AdiGarmendia <dejima@gmail.com>
2020-02-03 14:31:42 -0600

    add friend works

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -2,27 +2,75 @@ import React, { useContext } from "react";
 import Article from "./Article";
 import "./Article.css";
 import { ArticleContext } from "./ArticleProvider";
+import { FriendContext } from "../friends/FriendProvider";
 
 export default (props) => {
   const { articles } = useContext(ArticleContext)
+  const { friends} = useContext(FriendContext)
 
-  
+  let isFriend = false;
 
-  return (
-      <>
-          <h1>articles</h1>
+  const usersArticles = articles.filter(
+    article =>
+        article.userId === parseInt(localStorage.getItem("nutshell_user"))
+)
 
-          <button onClick={() => props.history.push("/articles/create")}>
-              Share Stuff
-          </button>
-          <div className="articles">
+  let friendsArticles = []
+    friends.map(friend => {
+        if (friend.initiatorId === parseInt(localStorage.getItem("nutshell_user"))) {
+            articles.filter(
+                article => {  
+                    if (article.userId === friend.user.id) {
+                        friendsArticles.push(article)
+                    }
+                }
+            )
+        }
+    })
 
-              {
-                  articles.map(article => {
-                      return <Article key={article.id} article={article} {...props} />
-                  })
+    const combinedArray = usersArticles.concat(friendsArticles)
+    const sortedCombinedArray = combinedArray.sort(function(a, b) {
+      return new Date(b.date) - new Date(a.date);
+    })
+
+    return (
+        <section className="ArticlesContainer">
+                <h2>Articles</h2>
+    
+           <button className="btn btn-primary" onClick={() => {
+                    props.history.push(`/articles/create`)
+                }}>Add Articles</button>
+            <div className="ArticlesSection">
+          {
+            sortedCombinedArray.map(singleArticles => {
+              if (singleArticles.userId != parseInt(localStorage.getItem("nutshell_user"))) {
+                isFriend = true;
+    
               }
-          </div>
-      </>
-  )
+                return (
+                    <Article props={props} key={singleArticles.id}
+                          Articles={singleArticles}
+                          friendStatus={isFriend} />
+                )
+            })
+          }</div>
+        </section>
+      )
+//   return (
+//       <>
+//           <h1>articles</h1>
+
+//           <button onClick={() => props.history.push("/articles/create")}>
+//               Share Stuff
+//           </button>
+//           <div className="articles">
+
+//               {
+//                   articles.map(article => {
+//                       return <Article key={article.id} article={article} {...props} />
+//                   })
+//               }
+//           </div>
+//       </>
+//   )
 }
